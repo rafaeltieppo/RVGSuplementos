7650287d76a3b6975d790099a99d5e0f680dd973
MiichaelJose <michaeljosecraft1@gmail.com>
2022-02-17 09:54:02 -0300

    att sql

--- Documentação/script.sql
+++ Documentação/script.sql
@@ -1,4 +1,4 @@
-DROP DATABASE IF NOT EXISTS suplementos;
+DROP DATABASE IF EXISTS suplementos;
 
 CREATE DATABASE IF NOT EXISTS suplementos;
 
@@ -6,7 +6,7 @@ USE suplementos;
 
 
 CREATE TABLE clientes(
-    id_cliente INTEGER NOT NULL PRIMARY KEY,
+    id_cliente INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(300) NOT NULL,
     senha VARCHAR(25) NOT NULL,
     email VARCHAR(100) NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE clientes(
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 CREATE TABLE administrador(
-    id_administrador INTEGER NOT NULL PRIMARY KEY,
+    id_administrador INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(300) NOT NULL,
     senha VARCHAR(25) NOT NULL,
     email VARCHAR(100) NOT NULL,
@@ -25,8 +25,8 @@ CREATE TABLE administrador(
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 CREATE TABLE suplementos(
-    id_suplemento INTEGER NOT NULL PRIMARY KEY,
-    id_cliente_pk INTEGER NOT NULL, 
+    id_suplemento INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_administrador INTEGER NOT NULL, 
     tipo VARCHAR(30) NOT NULL,
     nome VARCHAR(200) NOT NULL,
     preco INTEGER NOT NULL,
@@ -35,12 +35,35 @@ CREATE TABLE suplementos(
     imagem TEXT
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
 
+CREATE TABLE pedidos(
+    id_pedido INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_suplemento INTEGER NOT NULL,
+    id_cliente INTEGER NOT NULL, 
+    nome VARCHAR(200) NOT NULL,
+    preco INTEGER NOT NULL,
+    quantidade INTEGER NOT NULL,
+    imagem TEXT
+) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
 CREATE TABLE promocoes(
-    id_promocoes INTEGER NOT NULL PRIMARY KEY,
-    id_suplementos_pk INTEGER NOT NULL,
+    id_promocoes INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_suplemento INTEGER NOT NULL,
     porcentagem VARCHAR(300) NOT NULL,
     dias INTEGER NOT NULL
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
 
+ALTER TABLE pedidos ADD
+CONSTRAINT id_cliente_fk foreign key (id_cliente)
+REFERENCES clientes (id_cliente);
 
+ALTER TABLE pedidos ADD
+CONSTRAINT id_suplemento_fk foreign key (id_suplemento)
+REFERENCES suplementos (id_suplemento);
 
+ALTER TABLE suplementos ADD
+CONSTRAINT id_administrador_fk foreign key (id_administrador)
+REFERENCES administrador (id_administrador);
+
+ALTER TABLE promocoes ADD
+CONSTRAINT id_suplementos_fk foreign key (id_suplemento)
+REFERENCES suplementos (id_suplemento);
