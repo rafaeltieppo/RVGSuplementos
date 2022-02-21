8466bfdb16e9b296ddd5d81f31d077503462ab39
Gustavo Henrique <82879034+Gustavo010@users.noreply.github.com>
2022-02-21 11:26:34 -0300

    Add files via upload

---
+++ BackEnd(RVG)/Loja.sql
@@ -0,0 +1,46 @@
+
+Clientes: {
+    "id": 1,
+    "nascimento": 28/03/2007,
+    "nome": Vitor Tieppo,
+    "email": vitieppo@gmail.com,
+    "senha": guardaroupa123,
+    "imagem": fotooabcde,
+    "endereco": 13999-394,
+}
+
+Suplementos: {
+    "id": 1,
+    "tipo" Whey,
+    "nome": Rafael Leme,
+    "imagem": fotinhaabcd,
+    "quantidade": 2,
+    "lançamento": 31/03/2022,
+    "preco": 200, 
+}
+
+Admnistrador: {
+    "id": 1,
+    "nome": Gabriel Rocha,
+    "nascimento": 02/05/2005,
+    "email": gabrocha@gmail.com,
+    "senha": gab12345,
+}
+
+Promocoes: {
+    "id": 1,
+    "id_suplemento": 1,
+    "porcentagem": 20%,
+    "dias": 7,
+}
+
+Pedido: {
+    "id": 1,
+    "id_cliente":1,
+    "id_suplemento": 1,
+    "nome": Gusta Lemos,
+    "imagem": assdadadasadasd,
+    "preco": 200,
+    "quantidade": 2,
+}
+
