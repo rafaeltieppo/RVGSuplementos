fd4d9e83e8193782f1f759c329d78ee1e912f705
Rafael Tieppo <82878718+rafaeltieppo@users.noreply.github.com>
2022-04-01 11:30:03 -0300

    att

--- FrontEnd(RVG)/Home/index.js
+++ FrontEnd(RVG)/Home/index.js
@@ -6,8 +6,9 @@ var on = document.querySelector(".on");
 var data = localStorage.getItem('data');
 var descprod = document.querySelector(".descprod");
 var linhas = 0;
+var loginuser = document.querySelector('.loginuser');
 
-let loginuser = document.querySelector('.loginuser');
+
     loginuser.addEventListener("click", () => {
     window.location.href="../Login/index.html";
 })
@@ -49,65 +50,68 @@ function logout() {
     alert("Voçê saiu da sua conta");
 }
 
-async function carregarSuplementos(cont1 = 0) {
-    // const botao = document.querySelector('.botaos')
+// async function carregarSuplementos(cont1 = 0) {
+//      const butaos = document.querySelector('.botaos')
 
-    let url = "http://localhost/-banco-digital-/backend/src/controll/routes/route.suplementos.php/";
+//      let url = "http://localhost/-banco-digital-/backend/src/controll/routes/route.suplementos.php/";
 
-    // if(cont1 != null) url += "?cont1="+cont1;
-    
-    // console.log(url);
+//      if(cont1 != null) url += "?cont1="+cont1;
+     
+//      console.log(url);
+//      descprod.innerHTML = ""
+     
+//      const data = await fetch(url)
+//                  .then(resp => resp.json())
+//                  // criar cards
+//                  .then(data => {
+ 
+//                      data.arr.forEach(e => {
+//                          let p = document.createElement('p');
+//                          let preco = document.createElement('p');
+ 
+//                          p.innerHTML = e.nome;
+//                          preco.innerHTML = e.preco;
+ 
+ 
+//                          descprod.appendChild(p)
+//                          descprod.appendChild(preco)
 
-    const data = await fetch(url)
-                .then(resp => resp.json())
-                .then(data => {
-                    data.arr.forEach(e => {
-                        let p = document.createElement('p');
-                        let preco = document.createElement('p');
-
-                        p.innerHTML = e.nome;
-                        preco.innerHTML = e.preco;
-
-                        descprod.appendChild(p)
-                        descprod.appendChild(preco)
-
-                      
-                    });
-                    return data;
-                });
-
-    // linhas = data.numero;
-
-    // if(!botao.innerHTML.includes('button')) {
-    //     for (let i = 0; i < linhas; i++) {
-                   
-    //         let butao = document.createElement('button')
-                    
-    //         butao.addEventListener('click', async () => {
-    //              cont1 += 30;
-    //              await carregarSuplementos(cont1);
-    //         })
-        
-    //          butao.innerHTML = "2";
-    //          botao.appendChild(butao);
-    //      }
-                
-    //     } else {
-    //          console.log(botao);
-    //  }
-}
+//                      });
+                     
+//                      return data;
+//                  });
+ 
+//      linhas = data.numero;
+ 
+//      // contém botao na já adicionado ?
+//      if(!butaos.innerHTML.includes('button')) {
+         
+//          // criando os botões
+//          for (let i = 0; i < linhas; i++) {
+             
+//              let butao = document.createElement('button')
+//              butao.id = cont1;
+             
+//              butao.addEventListener('click', async () => {
+//                  await carregarSuplementos(butao.id);                
+//              })
+ 
+//              butao.innerHTML = "2";
+//              butaos.appendChild(butao);
+//              cont1 += 30;
+//          }
+//      } else {
+//          console.log(butaos);
+//      }
+// }
 
 function verNome() {
     if(localStorage.getItem('data') != null) {
         let pe = document.querySelector('.pentrar');
-        let espaco = document.querySelector('.loginuser');
-        pe.style.display = 'none';
-
-        let nome = document.createElement("p");
-        nome.innerHTML = "Olá" + localStorage.getItem(data.nome)
-        nome.className = "nomeh";
-
-        espaco.appendChild(nome);
+        let on = document.querySelector('.on')
+        pe.innerHTML = "Olá" + "\n" + localStorage.getItem('data').nome;
+        on.innerHTML = localStorage.getItem('data').nome;
+        
     }
 
 }
